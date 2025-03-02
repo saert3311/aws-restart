7cd7dc6d82d58ee20f0362f77111957f61422984
Jose Duarte <saert@outlook.com>
2025-03-02 16:55:56 +0000

    Till Exercise 6

--- Python/exercises.py
+++ Python/exercises.py
@@ -1,5 +1,6 @@
 #Various Python exercises
 #I am trying to use python annotations as good practices hehe
+from math import floor
 
 def initial() -> None:
     print("Start of exercises")
@@ -13,18 +14,57 @@ def exercise1() -> None:
     print(f'The 1st item from the list is {list[0]}')
     print(f'The last item from the list is {list[-1]}')
 
-def exercise2(list: list) -> None:
+def exercise2() -> None:
     welcome_msg(exercise2)
     list = [4, 5, 7]
     print(f'Initial list: {list}')
     list.extend([8, 9])
     print(f'Added items list: {list}')
-    #for fun lets 
-    middle_index = len(list)/2
+    middle_index = floor(len(list)/2)
+    list[middle_index] = 99 
+    print(f'Modified items list: {list}')
 
+def exercise3():
+    welcome_msg(exercise3)
+    dict = {
+        'Japan':'Tokio',
+        'Spain':'Madrid',
+        'Venezuela':'Caracas'
+    }
+    print(f'the capital of Spain is: {dict['Spain']}')
 
+def exercise4():
+    welcome_msg(exercise4)
+    dict = {
+        'name': 'Jose Duarte',
+        'age': 40,
+        'color':'blue'
+    }
+    print(f'Current dict: {dict}')
+    dict['food'] = 'Ramen'
+    print(f'Modified dict: {dict}')
+
+def exercise5():
+    welcome_msg(exercise5)
+    list = []
+    list.append('Orange')
+    list.append('Strawberry')
+    list.append('Pear')
+    list.insert(0, 'Apple')
+    print(f'Final list is {list}')
+
+def exercise6():
+    welcome_msg(exercise6)
+    list = ['Dog', 'Cat', 'Hamster', 'Monkey', 'Pigeon']
+    list.remove('Hamster')
+    deleted = list.pop()
+    print(f'The final list: {list}, and the last removed animal was {deleted}')
 
 if __name__ == "__main__":
     initial()
     exercise1()
     exercise2()
+    exercise3()
+    exercise4()
+    exercise5()
+    exercise6()
