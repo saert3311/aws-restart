9d2519c4af5b0669ff14c08cd69d2e5e198e3b49
Jose Duarte <saert@outlook.com>
2025-03-03 21:53:29 +0100

    Exercises 2 lab

--- Python Exercises 2/exercises.py
+++ Python Exercises 2/exercises.py
@@ -29,6 +29,56 @@ def exercise3(condition: str, temp: int) -> None:
     else:
         print('Insuficient data')
 
+def exercise4(username: str, password: str) -> None:
+    MYUSER = 'jhon'
+    MYPASSWORD = 'mypassword'
+
+    welcome_msg(exercise4)
+    if not username or not password:
+        print('Missing username or password')
+        return
+
+    if MYUSER == username:
+        if password == MYPASSWORD:
+            print('Correct credentials')
+        else:
+            print('Incorrect password')
+    else:
+        print('Incorrect username')
+
+def exercise5(age: int, have_membership: str='no') -> None:
+    welcome_msg(exercise5)
+
+    is_member = True if have_membership.lower() in ['y', 'yes'] else False
+    #if we want to give the best price we have to ask for ages 1st
+    #because a member of 8yo wont pay 10 insted of 5
+    if age < 12:
+        return print('Ticket price is 5$')
+    elif age >= 65:
+        return print('Ticket price is 8$')
+    elif is_member:
+        return print('Ticket price is 10$')
+    else:
+        return print('Ticket price is 15$')
+
+def exercise6() -> None:
+    welcome_msg(exercise6)
+    while True:
+        try:
+            price_input = int(input('Input product price:\n'))
+            break
+        except ValueError:
+            print('Product price must be an number')
+            continue
+
+    sale_input = input('Product is on Sale: (yes or y)\n')
+
+    is_onsale = True if sale_input.lower() in ['y', 'yes'] else False
+
+    sale_price = price_input - (price_input*0.25) if is_onsale else price_input
+
+    print(f'Original price: {price_input}, Sale price: {sale_price}')
+
 if __name__ == '__main__':
     initial()
     exercise1(9, 7)
@@ -41,3 +91,11 @@ if __name__ == '__main__':
     exercise3('rainy', 10)
     exercise3('sunny', 30)
     exercise3('sunny', 15)
+    exercise4('jhon', 'mypassword')
+    exercise4('jhon', 'another')
+    exercise4('may', 'another')
+    exercise5(10, 'yes')
+    exercise5(77, 'no')
+    exercise5(32, 'yes')
+    exercise5(21, 'no')
+    exercise6()
